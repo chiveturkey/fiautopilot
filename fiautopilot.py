cb0a03b0329d129280d7481e7627bae344a62d6f
eanderse <eanderse@rackspace.com>
2019-01-22 13:38:28 -0600

    Adding inflation field to principal_values and minor formatting change.

--- fiautopilot.py
+++ fiautopilot.py
@@ -15,7 +15,7 @@ def create_sqlite_db(dbname='fiautopilot.db'):
 
     # Create table.
     cursor.execute('''CREATE TABLE principal_values
-                      (date text, principal real)''')
+                      (date text, stock real, inflation real)''')
 
     # Save (commit) the changes, and close the connection.
     connection.commit()
@@ -32,7 +32,7 @@ def determine_when_stock_and_inflation_are_equal(principal_stock=0,
                                                  rate_stock=0.08,
                                                  number_of_compounds_per_years=12):
     time_in_years = (1 / number_of_compounds_per_years) * \
-                  (log(principal_stock/principal_inflation) /
-                   log((number_of_compounds_per_years + rate_inflation) /
-                       (number_of_compounds_per_years + rate_stock)))
+                    (log(principal_stock/principal_inflation) /
+                     log((number_of_compounds_per_years + rate_inflation) /
+                         (number_of_compounds_per_years + rate_stock)))
     return time_in_years
