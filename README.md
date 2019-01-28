9782ecc54c4950f9db141f559f43318375ce66a6
Your Name <you@example.com>
2019-01-27 20:41:45 -0600

    Adding config file and removing DB function.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # fiautopilot
+
+This project seeks to automate Financial Independence tasks.
+
+## Configuration File
+
+If you would like to override, default system constants, include a config.json file with the following format.
+
+```
+{
+  "principal_stock": 0,
+  "principal_inflation": 750000,
+  "rate_stock": 0.08,
+  "rate_inflation": 0.03,
+  "number_of_compounds_per_year": 12
+}
+```
--- fiautopilot.py
+++ fiautopilot.py
@@ -4,31 +4,37 @@ from math import log
 import matplotlib.pyplot as plot
 import numpy
 import sqlite3
+import os
+import sys
+import json
 
 
 # Constants
 
-PRINCIPAL_STOCK = 0
-PRINCIPAL_INFLATION = 750000
-RATE_STOCK = 0.08
-RATE_INFLATION = 0.03
-NUMBER_OF_COMPOUNDS_PER_YEAR = 12
+CONFIG_FILE = 'config.json'
 
+# If config.json exists, then use configuration values found there.  Otherwise,
+# use the following default constants.
 
-# Data Model
+if os.path.isfile(CONFIG_FILE):
+    try:
+        with open(CONFIG_FILE) as f:
+            config_data = json.load(f)
 
-# Create Database.
-def create_sqlite_db(dbname='fiautopilot.db'):
-    connection = sqlite3.connect(dbname)
-    cursor = connection.cursor()
-
-    # Create table.
-    cursor.execute('''CREATE TABLE principal_values
-                      (date text, stock real, inflation real)''')
-
-    # Save (commit) the changes, and close the connection.
-    connection.commit()
-    connection.close()
+        PRINCIPAL_STOCK = config_data['principal_stock']
+        PRINCIPAL_INFLATION = config_data['principal_inflation']
+        RATE_STOCK = config_data['rate_stock']
+        RATE_INFLATION = config_data['rate_inflation']
+        NUMBER_OF_COMPOUNDS_PER_YEAR = config_data['number_of_compounds_per_year']
+    except FileNotFoundError:
+        print(f'File {CONFIG_FILE} not found.')
+        sys.exit(1)
+else:
+    PRINCIPAL_STOCK = 50000
+    PRINCIPAL_INFLATION = 750000
+    RATE_STOCK = 0.08
+    RATE_INFLATION = 0.03
+    NUMBER_OF_COMPOUNDS_PER_YEAR = 12
 
 
 # Compound Interest Math
@@ -91,4 +97,4 @@ def create_stock_vs_inflation_graph(principal_stock=PRINCIPAL_STOCK,
     # Function to show the plots for both graphs
     plot.show()
 
-create_stock_vs_inflation_graph(50000)
+create_stock_vs_inflation_graph()
